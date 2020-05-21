1177a48e81f1f7babdff8f35b9a33fecbac42aef
Julián Palomo <julian@usemarkup.com>
2020-05-21 21:43:24 +0100

    Component updated

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,17 +1,5 @@
-import Head from "next/head";
+export const siteTitle = 'Julian Palomo';
 
-import styles from "./layout.module.scss";
-
-export const siteTitle = "app";
-
-const Layout = ({ children }) => (
-  <div className={styles.container}>
-    <Head>
-      <meta name="og:title" content={siteTitle} />
-    </Head>
-    <header></header>
-    <main>{children}</main>
-  </div>
-);
+const Layout = ({ children, home }) => <section>{children}</section>;
 
 export default Layout;
