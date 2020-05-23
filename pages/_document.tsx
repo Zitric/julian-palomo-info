131e5f6e7d97ded0862191e7ce2e2ab87f681fac
Julián Palomo <julian@usemarkup.com>
2020-05-23 19:04:08 +0100

    I'm trying to use particles js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,13 @@
+import * as React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-export default class CustomDocument extends Document {
+class CustomDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <script src="https://cdn.rawgit.com/progers/pathseg/master/pathseg.js"></script>
         </Head>
         <body>
           <Main />
@@ -15,3 +17,5 @@ export default class CustomDocument extends Document {
     );
   }
 }
+
+export default CustomDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout/layout';
 import { FunctionComponent } from 'react';
+import Particles from 'react-tsparticles';
+// import Particles from 'react-particles-js';
+// import { tsParticles } from 'tsparticles';
 
 const Home: FunctionComponent = () => (
   // <div className="container">
@@ -11,7 +14,13 @@ const Home: FunctionComponent = () => (
     <header>
       <h1>Hello world!</h1>
     </header>
-    <Layout home>Children</Layout>
+    <div>
+      <Particles />
+    </div>
+    <Layout>
+      {/* <tsParticles /> */}
+      Hola
+    </Layout>
   </div>
 );
 
